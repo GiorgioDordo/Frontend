70db1b7b2ecb73b8246c64ada66af86a1b969db3
GiorgioDordo <djole6789@gmail.com>
2025-05-08 20:11:41 +0200

    feat: Implement login and logout components with basic functionality
    - Added LoginComponent and LogoutComponent with respective HTML and CSS files.
    - Updated AppComponent to include login and logout components.
    - Modified app.component.html to display a welcome message and links to Angular resources.
    - Added interpolation method in AppComponent for demonstration.
    - Created unit tests for LoginComponent and LogoutComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
+import { LogoutComponent } from './core/logout/logout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, LogoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend';
+  message: string = 'Hello from Angular!';
+
+  InterpolationMethod() {
+    return 'Hello from Interpolation!';
+  }
 }
