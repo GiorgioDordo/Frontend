7c2abd9a4a87cf32085012f0dc58009899a97322
GiorgioDordo <djole6789@gmail.com>
2025-05-13 17:39:09 +0200

    feat: Add Binding component and update routing with new path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './core/login/login.component';
 import { CustomersComponent } from './features/customers/customers.component';
 import { ProductsComponent } from './features/products/products.component';
 import { LogoutComponent } from './core/logout/logout.component';
+import { BindingComponent } from './features/binding/binding.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'customers', component: CustomersComponent},
   {path: 'products', component: ProductsComponent},
   {path: 'logout', component: LogoutComponent},
+  {path: 'binding', component: BindingComponent}
 ];
---
+++ src/app/features/binding/binding.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  imports: [],
+  templateUrl: './binding.component.html',
+  styleUrl: './binding.component.css'
+})
+export class BindingComponent {
+
+  message: string = 'Hello from Angular!';
+
+  InterpolationString() {
+    return `Hello from Interpolation!`;
+  }
+
+  CheckStatus() {
+    alert('Hello from CheckStatus!');
+  }
+}
