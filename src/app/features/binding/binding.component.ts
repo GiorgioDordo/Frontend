bf9f0de311a0e1ccdb58d358c26ff7b958f06adb
Dordo(Giorgio) Dekanovic <djole6789@gmail.com>
2025-05-15 15:08:26 +0200

    feat: Add Test1 and Test2 components with basic structure and tests; enhance BindingComponent with two-way data binding

--- src/app/features/binding/binding.component.ts
+++ src/app/features/binding/binding.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './binding.component.html',
   styleUrl: './binding.component.css'
 })
 export class BindingComponent {
-
+// Interpolation String
   message: string = 'Hello from Angular!';
 
   InterpolationString() {
     return `Hello from Interpolation!`;
   }
 
+  // Event Binding
   CheckStatus() {
     alert('Hello from CheckStatus!');
   }
 
+  // Property Binding
     isButtonDisabled: boolean = true;
 
     CheckDisabled() {
       this.isButtonDisabled = !this.isButtonDisabled;
     }
+
+    // Two Way Binding
+    twoWay: string = 'Hello from Two Way Binding!';
 }
